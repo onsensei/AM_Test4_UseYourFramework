3cbfb088d35ad226d92fe8f998e3ac3158f0a77e
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-16 17:46:36 +0700

    add framework

---
+++ SimpleSwiftFramework/CallingFrameworkFunction.swift
@@ -0,0 +1,9 @@
+//
+//  CallingFrameworkFunction.swift
+//  SimpleSwiftFramework
+//
+//  Created by Thanaphat Suwannikornkul on 16/02/62 BE.
+//  Copyright © 2562 AppMan. All rights reserved.
+//
+
+import Foundation
---
+++ SimpleSwiftFramework/SimpleSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSwiftFramework.h
+//  SimpleSwiftFramework
+//
+//  Created by Thanaphat Suwannikornkul on 16/02/62 BE.
+//  Copyright © 2562 AppMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleSwiftFramework.
+FOUNDATION_EXPORT double SimpleSwiftFrameworkVersionNumber;
+
+//! Project version string for SimpleSwiftFramework.
+FOUNDATION_EXPORT const unsigned char SimpleSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSwiftFramework/PublicHeader.h>
+
+
