19188ba2c8c230a356381f636e7928ef4533a4bb
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-16 17:47:07 +0700

    make alert from framework

--- SimpleSwiftFramework/CallingFrameworkFunction.swift
+++ SimpleSwiftFramework/CallingFrameworkFunction.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+
+public class CallingFrameworkFunction: NSObject {
+    public static func alert(message: String) {
+        let alertController = UIAlertController(title: "Alert from Framework", message: "Your message is \"\(message)\"", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            //
+        }
+        alertController.addAction(action1)
+        
+        // -----
+        
+        var topController = UIApplication.shared.keyWindow!.rootViewController as! UIViewController
+        
+        while ((topController.presentedViewController) != nil) {
+            topController = topController.presentedViewController!;
+        }
+        topController.present(alertController, animated:true, completion:nil)
+    }
+}
